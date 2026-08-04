1985ce4bb2838a92eddb5bd2196a0437e5e555b0
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2026-08-04 13:53:46 -0600

    Initial commit

---
+++ src/main/java/com/pablocompany/practica/no1/compi2/PracticaNo1Compi2.java
@@ -0,0 +1,12 @@
+package com.pablocompany.practica.no1.compi2;
+
+/**
+ *
+ * @author pablo03
+ */
+public class PracticaNo1Compi2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
